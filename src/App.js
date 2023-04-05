4ef697a218c680098396530464d1d5fd59dd08e5
badat2k00 <75111242+badat2k00@users.noreply.github.com>
2023-04-05 16:50:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,17 +22,27 @@ function App() {
   return (
    <div className="flex justify-center items-center bg-black h-screen">
     <div className="px-5 pt-2.5 pb-5 w-2/5 h-50 bg-white">
+    <form onSubmit={(e) => {
+              e.preventDefault();
+            }}> 
       <div className="flex justify-center items-center gap-x-2.5 w-full">
-        <input className=" shrink grow basic-auto bg-neutral-200 h-7.5  rounded-md px-2.5 py-0" type="text" placeholder="Nhập tên" />
-        <input  className=" shrink grow basic-auto bg-neutral-200 h-7.5 rounded-md px-2.5 py-0"type="text " placeholder="Nhập số điện thoại" />
+        <input className=" shrink grow basic-auto bg-neutral-200 h-7.5  rounded-md px-2.5 py-0" type="text" placeholder="Nhập tên" onChange={(e) => {
+                console.log(e.target.value);
+              }} />
+        <input  className=" shrink grow basic-auto bg-neutral-200 h-7.5 rounded-md px-2.5 py-0"type="text " placeholder="Nhập số điện thoại" onChange={(e) => {
+                console.log(e.target.value);
+              }} />
         <button className="shrink grow basic-auto text-white bg-blue-800 h-7.5 rounded-md">THÊM</button>      
       </div>
       <div className="h-px bg-gray-400 w-full my-2.5 mx-0 "/>
       <div className="flex justify-center items-center w-full gap-x-2.5">
-      <input  className=" shrink grow basic-auto bg-neutral-200 px-2.5 py-0 h-7.5 rounded-md" type="text" placeholder="Tìm kiếm" />
+      <input  className=" shrink grow basic-auto bg-neutral-200 px-2.5 py-0 h-7.5 rounded-md" type="text" placeholder="Tìm kiếm" onChange={(e) => {
+                console.log(e.target.value);
+              }}/>
         <button className="shrink grow basic-auto text-white bg-blue-800  h-7.5 rounded-md">TÌM</button>
         <button className="shrink grow basic-auto text-white bg-blue-800  h-7.5 rounded-md"> XÓA TRÙNG</button>     
       </div>
+      </form>
       <div className="h-50 flex mt-2.5 flex-col gap-y-3.5 ">
         {
           array.map((value) =>(
